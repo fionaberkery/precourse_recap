28723cb52893049281e6aedaf4b37895169bee80
Fiona <fiona_berkery@hotmail.com>
2022-03-28 16:30:46 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,14 @@
+number_1 = 10
+number_2 = 12
+print(number_1 * number_2)
+sum = number_1 + number_2
+print(sum)
+number_1 = 2
+
+sum = (number_1 + number_2)
+print(sum)
+
+my_name = "Fiona"
+my_home = "Edinburgh"
+
+print(my_name + " " + "I live in " + my_home)
